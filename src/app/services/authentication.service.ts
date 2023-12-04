139e4e7a54278f97a57b47387dc9efabad067f85
Yunic10 <yudithnico99@gmail.com>
2023-12-05 06:52:13 +0700

    lupa up github

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+const TOKEN_KEY = 'token-saya';
+@Injectable({
+ providedIn: 'root'
+})
+export class AuthenticationService {
+ constructor(private http: HttpClient) {
+ this.loadToken();
+ }
+  loadToken() {
+  return localStorage.getItem(TOKEN_KEY);
+}
+apiURL() {
+ return "https://api-auth.dalhaqq.xyz/";
+}
+ logout() {
+  localStorage.clear();
+  return true;
+}
+}
